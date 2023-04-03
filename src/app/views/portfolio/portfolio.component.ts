000fadf4e65a897f2329887b0a30ecfd0c0df773
yunusyerli1 <yunusyerli1@gmail.com>
2023-04-03 15:46:08 +0300

    Add portfolio list

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  projectList: any = [
+    {
+      title: "SuiteCrm",
+      text:  "Mobile App",
+      image: "../../../assets/img/suitecrm.png",
+      platform: "Website",
+      link: "https://suitecrm.com/"
+    },
+    {
+      title: "Gaboras",
+      text:"Website",
+      image: "../../../assets/img/gaboras.jpg",
+      platform: "Website",
+      link: "https://www.gaboras.com.tr",
+    },
+    {
+      title: "Gaboras Mobile",
+      text:  "Mobile App",
+      image: "../../../assets/img/gaboras-mobile.png",
+      platform: "Mobile App",
+      link: "https://apps.apple.com/tr/app/gaboras-mobil/id1644123394"
+    },
+    {
+      title: "Alsayyed Design",
+      text:  "Alsayyed Design Website",
+      image: "../../../assets/img/alsayyeddesign.png",
+      platform: "Website",
+      link: "https://alsayyeddesign.com/"
+    },
+    {
+      title: "Gameon",
+      text:  "Gameon Website",
+      image: "../../../assets/img/games.png",
+      platform: "Website",
+      link: "https://github.com/yunusyerli1/gameon"
+    },
+    {
+      title: "Beach Resort",
+      text:  "Beach Resort Website",
+      image: "../../../assets/img/beachresort.jpg",
+      platform: "Website",
+      link: "https://github.com/yunusyerli1/beachresort"
+    },
+    {
+      title: "Amazona",
+      text:  "Amazona Website",
+      image: "../../../assets/img/shopping.jpg",
+      platform: "Website",
+      link: "https://github.com/yunusyerli1/shoppingCart"
+    }
+
+  ];
+
 }
