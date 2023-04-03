7e788844a95c2417a7b925ce0f2c45a9378fe667
yunusyerli1 <yunusyerli1@gmail.com>
2023-04-03 16:40:56 +0300

    Refactor portfolio list

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -10,49 +10,49 @@ export class PortfolioComponent {
   projectList: any = [
     {
       title: "SuiteCrm",
-      text:  "Mobile App",
+      text:  "Web Development",
       image: "../../../assets/img/suitecrm.png",
       platform: "Website",
       link: "https://suitecrm.com/"
     },
     {
       title: "Gaboras",
-      text:"Website",
+      text:"Web Development",
       image: "../../../assets/img/gaboras.jpg",
       platform: "Website",
       link: "https://www.gaboras.com.tr",
     },
     {
       title: "Gaboras Mobile",
-      text:  "Mobile App",
+      text:  "Mobile App Development",
       image: "../../../assets/img/gaboras-mobile.png",
       platform: "Mobile App",
       link: "https://apps.apple.com/tr/app/gaboras-mobil/id1644123394"
     },
     {
       title: "Alsayyed Design",
-      text:  "Alsayyed Design Website",
+      text:  "Web Development",
       image: "../../../assets/img/alsayyeddesign.png",
       platform: "Website",
       link: "https://alsayyeddesign.com/"
     },
     {
       title: "Gameon",
-      text:  "Gameon Website",
+      text:  "Web Development",
       image: "../../../assets/img/games.png",
       platform: "Website",
       link: "https://github.com/yunusyerli1/gameon"
     },
     {
       title: "Beach Resort",
-      text:  "Beach Resort Website",
+      text:  "Web Development",
       image: "../../../assets/img/beachresort.jpg",
       platform: "Website",
-      link: "https://github.com/yunusyerli1/beachresort"
+      link: "https://react-kemer-beach-resort.netlify.app/"
     },
     {
       title: "Amazona",
-      text:  "Amazona Website",
+      text:  "Web Development",
       image: "../../../assets/img/shopping.jpg",
       platform: "Website",
       link: "https://github.com/yunusyerli1/shoppingCart"
